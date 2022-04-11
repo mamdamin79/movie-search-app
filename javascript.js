9cd9b1095f19c044e72b9bd1a75906a6f84f8e89
mohammadaminsaheb <mohammadaminsaheb@gmail.com>
2022-04-11 09:06:42 -0700

    create ui staticly

---
+++ javascript.js
@@ -0,0 +1,14 @@
+// selectors
+const resultContainer = document.querySelector(".result-container");
+const searchInput = document.querySelector(".search-input");
+const searchList = document.querySelector(".search-list");
+
+
+// global variables
+let movieLoadedArray = [];
+
+
+
+
+            
+
